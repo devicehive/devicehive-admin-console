0c2e6876422d649f82dfcc3cd371235cacdbfb24
Mikhail Shatalov <mshatalov@dataart.com>
2013-10-25 20:27:36 +0400

    Updated domains and networks handling on access key permission edit

--- Scripts/Views/AccessKey.js
+++ Scripts/Views/AccessKey.js
@@ -4,7 +4,11 @@ app.Views.AccessKeyPermissionEditListItem = Backbone.Marionette.ItemView.extend(
         "click .remove-network": "removeNetwork",
         "click .remove-device": "removeDevice",
         "click .add-device": "addDevice",
-        "click .add-network": "addNetwork"
+        "click .add-network": "addNetwork",
+        "change .subnets": "changeSubnets",
+        "keyup .subnets": "changeSubnets",
+        "change .domains": "changeDomains",
+        "keyup .domains": "changeDomains"
     },
     triggers: {
         "click .remove-access-key-permission": "removePermission"
@@ -14,7 +18,15 @@ app.Views.AccessKeyPermissionEditListItem = Backbone.Marionette.ItemView.extend(
     initialize: function(options) {
         this.networks = options.networks;
         this.devices = options.devices;
+        /*this.bindTo(this.model, "change:domains", this.prepareViewModel);
+        this.bindTo(this.model, "change:subnets", this.prepareViewModel);*/
         this.bindTo(this.model, "change", this.render);
+        this.prepareViewModel();
+    },
+    prepareViewModel: function() {
+        console.log("prepare");
+        this.domainsText = (this.model.get("domains") || []).join("\n");
+        this.subnetsText = (this.model.get("subnets") || []).join("\n");
     },
     serializeData: function() {
         var data = this.model.toJSON({ escape: true });
@@ -25,11 +37,10 @@ app.Views.AccessKeyPermissionEditListItem = Backbone.Marionette.ItemView.extend(
         initArray(data, ["domains", "subnets", "actions", "networkIds", "deviceGuids"]);
         data.networks = this.networks;
         data.devices = this.devices;
+        data.domainsText = this.domainsText;
+        data.subnetsText = this.subnetsText;
         data.availableActions = app.Models.AccessKeyPermission.actions;
         return data;
-    },
-    updateModel: function() {
-        return true;
     },
     addNetwork: function() {
         var id = parseInt(this.$el.find("select.networks").val());
@@ -61,21 +72,35 @@ app.Views.AccessKeyPermissionEditListItem = Backbone.Marionette.ItemView.extend(
             networkIds.splice(index, 1);
             if (_.isEmpty(networkIds))
                 this.model.set("networkIds", null);
-            this.change();
+            this.change("networkIds");
         }
     },
     removeDevice: function(el) {
-        var id = $(el.target).closest("li").data("id")
+        var id = $(el.target).closest("li").data("id");
         var deviceGuids = this.model.get("deviceGuids");
         var index = _.indexOf(deviceGuids, id);
         if (index != -1) {
             deviceGuids.splice(index, 1);
             if (_.isEmpty(deviceGuids))
                 this.model.set("deviceGuids", null);
-            this.change();
+            this.change("deviceGuids");
         }
     },
-    change: function() {
+    changeDomains: function() {
+        this.domainsText = this.$el.find("textarea.domains").val();
+        var domainsList = _.without(this.domainsText.split("\n").map(function(domain) { return $.trim(domain); }), "");
+        domainsList = _.isEmpty(domainsList) ? null : domainsList;
+        this.model.set("domains", domainsList, { silent: true });
+    },
+    changeSubnets: function() {
+        this.subnetsText = this.$el.find("textarea.subnets").val();
+        var subnetsList = _.without(this.subnetsText.split("\n").map(function(subnet) { return $.trim(subnet); }), "");
+        subnetsList = _.isEmpty(subnetsList) ? null : subnetsList;
+        this.model.set("subnets", subnetsList, { silent: true });
+    },
+    change: function(attr) {
+        if (attr && !_.isEmpty(attr))
+            this.model.trigger("change:" + attr, this.model);
         this.model.trigger("change", this.model);
     }
 });
@@ -107,6 +132,16 @@ app.Views.AccessKey = Backbone.Marionette.CompositeView.extend({
             devices: _.reduce(this.devices.toJSON(), function(obj, device) { obj[device.id] = device; return obj; }, {})
         };
     },
+    onRender: function() {
+        this.$el.find(".expiration-date").datetimepicker({
+            onClose: function (dateText) {
+                //if (!_.isEmpty(dateText))
+                    //that.model.set("startDate", that.parseDate(dateText));
+                //else
+                    //that.model.set("startDate", null);
+            }
+        });
+    },
     saveAccessKey: function() {
         //app.vent.trigger("notification", app.Enums.NotificationType.Error, "hello :)");
     },
@@ -115,6 +150,12 @@ app.Views.AccessKey = Backbone.Marionette.CompositeView.extend({
     },
     removePermission: function(viewItem) {
         this.model.get("permissions").remove(viewItem.model);
+    },
+    updateModel: function() {
+        var expDate = null;
+        if (!_.isEmpty(this.$el.find(".expiration-date").val()))
+            expDate = this.$el.find(".expiration-date").datetimepicker("getDate");
+        this.model.set("expirationDate", expDate);
     }
 
 });
