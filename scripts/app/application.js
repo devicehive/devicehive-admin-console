ef44aa7e42811fdbc58e2ba8979a6cb600dfc34a
Sergey Popov <sergey.popov@dataart.com>
2014-10-14 20:58:05 +0300

    wip: logout and changepassword functionality works. TODO: arrange functionaltity to separate controllers

--- scripts/app/application.js
+++ scripts/app/application.js
@@ -122,11 +122,18 @@ app.bind("initialize:after", function (options) {
     }
 });
 
+app.bind('launch', function() {
+    console.log('app launch?');
+    app.Regions.statusArea.show(new app.Views.authHeader(app.User));
+});
+
 app.bind("login", function (options) {
     console.log('login event');
     // fetch current user
     app.User.fetch({ success: function() {
         if (app.User.id != null) {
+            app.trigger('launch');
+            console.log('trigger navigatedTo');
             Backbone.history.trigger("navigatedTo", Backbone.history.getFragment());
         }
     },
@@ -148,8 +155,14 @@ app.Views.Login = Backbone.Marionette.ItemView.extend({
     template: 'user-login',
     onRender: function() {
         console.log('render login view this %o, el, %o', this, this.$el);
+        var context = this;
+        function showError(message) {
+            context.$el.find('form .error').html(message);
+            context.$el.find('form .error').toggleClass('ui-helper-hidden', !message);
+        };
         this.$el.find('form').on('submit', function(e) {
             e.preventDefault();
+            showError(''); // clear error message
             console.log('form submit event %o', e);
             var login = $(e.target).find('[name=login]').val();
             var password = $(e.target).find('[name=password]').val();
@@ -166,10 +179,10 @@ app.Views.Login = Backbone.Marionette.ItemView.extend({
                 },
                 error: function(resp, status) {
                     console.log('fail %o', resp);
-                    if (status == 401) {
-                        alert('Invalid login or password');
+                    if (resp.status == 401) {
+                        showError('Wrong login or password');
                     } else {
-                        alert('Server error');
+                        showError('Server error: '+resp.responseText);
                     }
                 }
             };
@@ -180,15 +193,120 @@ app.Views.Login = Backbone.Marionette.ItemView.extend({
 });
 
 
+
+app.Views.authHeader = Backbone.Marionette.ItemView.extend({
+    template: 'auth-header',
+    initialize: function(user) {
+        this.userLogin = user.get('login');
+    },
+    serializeData: function() {
+        return {userLogin: this.userLogin};
+    },
+    onRender: function() {
+        console.log('render auth header');
+    },
+    events: {
+        "click a": "clickHandler"
+    },
+    clickHandler: function(e) {
+        console.log('click handler option %o', e);
+        e.preventDefault();
+        $el = $(e.target);
+        Backbone.history.navigate($el.data('path'), { trigger: true });
+    }
+});
+
+app.Views.ChangePassword = Backbone.Marionette.ItemView.extend({
+    template: 'change-password',
+    onRender: function() {
+        console.log('render change password');
+        var context = this;
+        function error(message) {
+            context.$el.find('form .error').html(message);
+            context.$el.find('form .error').toggleClass('ui-helper-hidden', !message);
+        };
+        function success(newpassword) {
+            context.$el.find('form .success').toggleClass('ui-helper-hidden', !newpassword);
+            if (newpassword) {
+                sessionStorage.userPassword = newpassword;
+                sessionStorage.lastActivity = (new Date()).valueOf();
+                context.$el.find('form .fields').hide();
+            }
+        }
+
+        this.$el.find('form').on('submit', function(e) {
+            e.preventDefault();
+            console.log('form submit event %o', e);
+            var login = sessionStorage.userLogin;
+            var $form = $(e.target);
+            error(''); // clear error message
+            var currentpassword = $form.find('[name=currentpassword]').val();
+            var newpassword = $form.find('[name=password]').val();
+            var confirmpassword = $form.find('[name=confirmpassword]').val();
+            if ( !currentpassword ) {
+                error('Please specify current password');
+                return;
+            }
+            if ( !newpassword ) {
+                error('Please specify new password');
+                return;
+            }
+            if ( newpassword != confirmpassword ) {
+                console.log('newpassword %o, confpassword %o', newpassword, confirmpassword);
+                error('New password and confirm password do not match');
+                return;
+            }
+            // perform check of current password
+            var headers = {'Authorization': 'Basic '+btoa(login+':'+currentpassword)}; 
+            var options = {
+                headers: headers,
+                success: function(resp, status) {
+                    console.log('success %o', resp);
+                    // try to set new password
+                    $.ajax(app.User.urlCurrent(), {
+                        headers: headers,
+                        type: 'PUT',
+                        data: { password: newpassword },
+                        success: function(resp) {
+                            console.log('successfully changed password');
+                            success(newpassword);
+                        },
+                        error: function(resp) {
+                            console.log('error changing password %o %o', resp, arguments);
+                            var responseObject = JSON.parse(resp.responseText);
+                            if (responseObject.message) {
+                                error(responseObject.message);
+                            } else {
+                                error('Server error '+resp.status+': '+resp.responseText);
+                            }
+                        }
+                    });
+                },
+                error: function(resp) {
+                    console.log('fail %o', resp);
+                    if (resp.status == 401) {
+                        error('Current password incorrect');
+                    } else {
+                        error('Server error');
+                    }
+                }
+            };
+            $.ajax(app.User.urlCurrent(), options);
+        });
+    }
+});
+
+
 app.module("Modules.Login", function (users, app) {
     console.log("Modules.Login users %o, app %o", users, app);
     var loginView = new app.Views.Login();
+    var changePasswordView = new app.Views.ChangePassword();
 
     var controller = {
         'auth': function() {
             if (sessionStorage.userLogin && sessionStorage.userPassword) {
                 console.log('auth action redirects to / because there are some credentials');
-                Backbone.history.navigate('', {trigger: false});
+                Backbone.history.navigate('', {trigger: true});
                 return;
             } else {
                 console.log('credentials incomplete or missing');
@@ -200,19 +318,20 @@ app.module("Modules.Login", function (users, app) {
             delete sessionStorage.userPassword;
             Backbone.history.navigate('', { trigger: false });
             location.reload(true);
+        },
+        'changepassword': function() {
+            app.Regions.topWorkArea.show(changePasswordView);
         }
     };
     var routes = {
         auth: 'auth',
-        logout: 'logout'
+        logout: 'logout',
+        changepassword: 'changepassword'
     };
     var router = Backbone.Marionette.AppRouter.extend({ controller: controller, appRoutes: routes });
     app.addInitializer(function (options) {
         console.log('inited module.login and rtr is started');
         var rtr = new router();
-    });
-    app.bind("initialize:after", function () {
-        app.vent.trigger("addResource", "auth", "Login", []);
     });
 
 });
