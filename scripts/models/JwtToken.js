b0b4937993ca3c5a9f573839917fcbef04160e6f
Natalia Boldinova <Natalia.Boldinova@dataart.com>
2016-12-14 17:44:42 +0300

    Minor jwt parsing fix

--- scripts/models/JwtToken.js
+++ scripts/models/JwtToken.js
@@ -1,8 +1,12 @@
 app.Models.JwtToken = Backbone.Model.extend({
     initialize: function (params) {
         localStorage.deviceHiveToken = params.accessToken;
-        var parsedJwt = parseJwt(params.accessToken);
-        localStorage.expiration = parsedJwt.payload.expiration;
+        try {
+            var parsedJwt = parseJwt(params.accessToken);
+            localStorage.expiration = parsedJwt.payload.expiration;
+        } catch (err) {
+            console.log(err);
+        }
         var appUrl = app.f.prepareAbsolutePath(app.config.rootUrl);
         localStorage.lastActivity = (new Date()).valueOf();
         location.href = appUrl;
@@ -13,4 +17,4 @@ function parseJwt(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace('-', '+').replace('_', '/');
     return JSON.parse(window.atob(base64));
-};
+}
