d46839389bc157bcab7aea423bf51a1c970f92bc
Dima Ananskyi <starkandtesla@gmail.com>
2017-06-02 12:59:02 +0300

    User validation

--- scripts/views/UserCreateEdit.js
+++ scripts/views/UserCreateEdit.js
@@ -49,6 +49,48 @@ app.Views.UserCreateEdit = Backbone.Marionette.ItemView.extend({
         var status = parseInt(this.$el.find("#status :selected").val());
         var data = this.$el.find("#data").val();
 
+        //Start user form validation
+        var loginPatt = /^[a-zA-Z0-9._@]{3,128}$/;
+
+        if(!login || (login.length < 3) ||  (login.length  > 128)) {
+            this.$el.find('#login-length-error').show();
+            return;
+        } else if (!loginPatt.test(login)) {
+            this.$el.find('#login-length-error').hide();
+            this.$el.find('#login-pattern-error').show();
+            return;
+        } else {
+            this.$el.find('#login-length-error').hide();
+            this.$el.find('#login-pattern-error').hide();
+        }
+
+
+        var pass = this.$el.find("#password").val();
+        var passConf = this.$el.find("#password-confirmation").val();
+
+        if (!pass || pass.length < 6 || pass.length > 128) {
+            this.$el.find('#password-length-error').show();
+            return;
+        } else {
+            this.$el.find('#password-length-error').hide();
+        }
+
+
+        if (pass !== passConf) {
+            this.$el.find('#password-confirmation-match-error').show();
+            return;
+        } else {
+            this.$el.find('#password-confirmation-match-error').hide();
+        }
+
+        if((data.length > 0) && !app.isJson(data)) {
+            this.$el.find('#data-format-error').show();
+            return;
+        } else {
+            this.$el.find('#data-format-error').hide()
+        }
+        //End user form validation
+
         var googleLogin = this.$el.find("#googleLogin").val();
         if (_.isEmpty(googleLogin)) {
             googleLogin = null;
@@ -65,25 +107,12 @@ app.Views.UserCreateEdit = Backbone.Marionette.ItemView.extend({
             login: login,
             role: role,
             status: status,
+            password: pass,
+            data: data,
             googleLogin: googleLogin,
             facebookLogin: facebookLogin,
             githubLogin: githubLogin
         };
-
-        var pass = this.$el.find("#password").val();
-        var passConf = this.$el.find("#password-confirmation").val();
-
-        if (!_.isEmpty(pass)) {
-            if (pass != passConf) {
-                var message = "Pasword confirmation isn't match password";
-                app.vent.trigger("notification", app.Enums.NotificationType.Error, message);
-                return;
-            }
-            options.password = pass;
-        } else  {
-            app.vent.trigger("notification", app.Enums.NotificationType.Error, "Valid javascript object should be entered");
-            return;
-        }
 
         var that = this;
         this.model.save(options, { error: this.onSaveFail, success: function (model, response) { that.onSaveSuccess(model, response); }, wait:true });
